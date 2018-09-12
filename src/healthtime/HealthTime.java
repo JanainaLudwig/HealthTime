5cc8e905c1d29c3d796726f9aea34fec4a1c1a8b
JanainaLudwig <janainaludwig@alunos.utfpr.edu.br>
2018-09-12 19:22:58 -0300

    Remove of unused files

--- src/healthtime/HealthTime.java
+++ src/healthtime/HealthTime.java
@@ -5,11 +5,14 @@
  */
 package healthtime;
 
+import dashboard.view.DashboardMonthController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import utils.ControllerUtils;
 
 import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
@@ -20,11 +23,23 @@ public class HealthTime extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
+        //Starts at login
+        /*
         Parent root = FXMLLoader.load(getClass().getResource("/login/view/Login.fxml"));
-        //Parent root = FXMLLoader.load(getClass().getResource("/dashboard/view/DashboardMonth.fxml"));
         Scene scene = new Scene(root);
 
 
+        stage.setScene(scene);
+        stage.show();
+        */
+        //Starts at month view
+        DashboardMonthController controller = new DashboardMonthController(1);
+
+        FXMLLoader fxmlLoader = new FXMLLoader(controller.getClass().getResource("../../dashboard/view/DashboardMonth.fxml"));
+        fxmlLoader.setController(controller);
+        Parent root = fxmlLoader.load();
+
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
     }
